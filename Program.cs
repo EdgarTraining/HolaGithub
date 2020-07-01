b1dfb3c24511d3e4ec57d6e841a3d4efa747c343
Henrry Velez (Esage Group LLC) <v-hevele@microsoft.com>
2020-07-01 18:49:46 -0500

    Cambio HV

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+// HV modification
 namespace WebMartes
 {
     // Nuevos cambios despues de crear el clon en github
