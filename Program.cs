c4de6202a2615674b81ff6fbffe9d067facb87f7
Guillermo Andrés Layedra Larrea <g_layedra@hotmail.com>
2020-07-01 19:04:41 -0500

    Cambio GLL

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+//Modified GLL
 namespace WebMartes
 {
     // Nuevos cambios despues de crear el clon en github
