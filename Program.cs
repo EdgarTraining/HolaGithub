b5db7a9c5f905b8909d242f498ba91085f8af860
Henrry Velez (Esage Group LLC) <v-hevele@microsoft.com>
2020-07-01 19:14:11 -0500

    Cambio HeVe 3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-// Mijail osorio Repo Clone
-// El que hace Push Gana.... 
-// Modifican en busca de Conflictos
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +6,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-// HV modification
+// Henry Velez modification la tuca
 namespace WebMartes
-// Comentar lo mio para evitar conflicto por el momento
-//{ // Conflicto en camino con César
-{//César Betancurth
-//Carlos Silva
-    // Nuevos cambios despues de crear el clon en github
-    // Estos cambios *eventualmente* se van a ir al clon
-    public class Program // cambio conflictivo en master
+{
+       public class Program // cambio conflictivo en master
     {
         public static void Main(string[] args)
         {
-			//Andrea Rueda Conflito con Andrea (Mijail)
             CreateHostBuilder(args).Build().Run();
         }
-        //Alex Morales
-        // otro cambio conflictivo en master
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        //Name: Juan Moyano
     }
 }
