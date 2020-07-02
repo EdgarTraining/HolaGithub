65fc24ccc40015e635953328833488e545fe1eab
morasislema <albyron93@hotmail.com>
2020-07-01 19:07:31 -0500

    merge

--- Program.cs
+++ Program.cs
@@ -9,14 +9,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-<<<<<<< HEAD
+
 // HV modification
-=======
+
 
 // Édgar Sánchez Gordón
->>>>>>> 5675498157ccf74aecf58fdb49c71428468c5311
+ 
 namespace WebMartes
-{//César Betancurth
+{
+    //César Betancurth
     // Nuevos cambios despues de crear el clon en github
     // Estos cambios *eventualmente* se van a ir al clon
     public class Program // cambio conflictivo en master
