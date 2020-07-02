1fecd3c9ee2fbf6ab6743b9d82149958341d0aef
Édgar Sánchez Gordón <versioncontroltraining2@outlook.com>
2020-07-01 19:04:38 -0500

    Conflicto con César

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 // Édgar Sánchez Gordón
 namespace WebMartes
-{
+{ // Conflicto en camino con César
     // Nuevos cambios despues de crear el clon en github
     // Estos cambios *eventualmente* se van a ir al clon
     public class Program // cambio conflictivo en master
